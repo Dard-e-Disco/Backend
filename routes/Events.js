31abf44e9548cf6b4668860262f3732dd1a21500
dev-064 <divyanshujha112@gmail.com>
2023-01-21 17:00:04 +0530

    .

--- routes/Events.js
+++ routes/Events.js
@@ -6,17 +6,40 @@ const Router=express.Router()
 const { body, validationResult } = require('express-validator');
 const fetchuser = require('../middleware/fetchuser');
 const JWT= "Login@userToken"
-
-//creating user
-Router.get('/getAllEvents',
-fetchuser,async (req,res)=>{
-    
-    
+const Events = require('../models/Events');
+//getting events
+Router.get('/getAllEvents',async (req,res)=>
+{
+    try {
+        const Event = await Events.find();
+        res.send(Event);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
 })
-Router.post('/PostEvent',fetchuser,
+//postingEvents
+Router.post('/PostEvent',fetchuser,[
+    body('EventName', 'Enter a valid EventName').isLength({ min: 1 }),
+    body('Description', 'Description must be atleast 20 characters').isLength({ min: 20 }),
+    body('ParticipantsNumber', 'Description must be atleast 5 characters').isNumeric({min:1})
+],
 async(req,res)=>{
-  
-
+    try {
+        const{EventName,Desc,ParticipantNumber}=req.body;
+        // If there are errors, return Bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const Event = new Events({
+            EventName, Desc, ParticipantNumber
+        })
+        const savedEvent = await Event.save()
+        res.json(savedEvent);
+    } catch (error) {
+        res.status(500).send("Internal Server Error")
+    }
 }
 )
 

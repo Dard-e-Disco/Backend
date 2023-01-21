8f9735c3bdd20380014d8b0dad1e164fdea6b4e4
dev-064 <divyanshujha112@gmail.com>
2023-01-21 15:50:11 +0530

    .

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongooseURI = "mongodb+srv://cluster1.tizixwp.mongodb.net/myFirstDatabase"
+const mongooseURI = "mongodb+srv://dev064:Adityajha%4011@cluster1.tizixwp.mongodb.net/?retryWrites=true&w=majority"
 const startdb=()=>{
     mongoose.connect(mongooseURI)
     console.log("Database is connsdected succesfully")

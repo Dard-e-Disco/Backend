a3115bddb3245ea6b36491e7a84d7a8ac0d7838e
rraj-03 <ritikraj1369@gmail.com>
2023-01-21 16:04:41 +0530

    ,.

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
-const mongooseURI = "mongodb+srv://dev064:Adityajha%4011@cluster1.tizixwp.mongodb.net/?retryWrites=true&w=majority"
+const mongooseURI = "mongodb+srv://dev064:Adityajha%4011@cluster1.tizixwp.mongodb.net/ktj_test?retryWrites=true&w=majority"
 const startdb=()=>{
     mongoose.connect(mongooseURI)
+    mongoose.connection.useDb("admin")
     console.log("Database is connsdected succesfully")
 }
 module.exports = startdb;
